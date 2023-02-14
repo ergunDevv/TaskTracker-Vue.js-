102131e50d17870f0a1883d63e2fcbe719303e15
ergunDevv <ergncyln2127088@gmail.com>
2023-02-14 19:19:22 +0300

    Multipage functionality added with routes

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+
+        <h1>Version 1.0.0</h1>
+        <router-link to="/">Go Back</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AboutComponent"
+}
+</script>
